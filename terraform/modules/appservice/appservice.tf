84e9e687b4e509ce4b93ef04207a46552f7fe85d
unknown <nguyen.huu.luong.tu@gmail.com>
2022-12-22 21:48:10 +0700

    deploy fakerestapi

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,24 +1,20 @@
-resource "azurerm_service_plan" "test" {
+resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Windows"
-  sku_name            = "F1"
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
 
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_app_service" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  service_plan_id     = azurerm_service_plan.test.id
+  app_service_plan_id = azurerm_app_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
-  }
-  site_config {
-    always_on = false
-    application_stack {
-      dotnet_version = "7.0"
-    }
   }
 }
