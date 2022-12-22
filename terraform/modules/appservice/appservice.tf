cfe5e7389cc3358336a98a8c0d24dd239f67153c
unknown <nguyen.huu.luong.tu@gmail.com>
2022-12-22 20:50:01 +0700

    deploy fakerestapi

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,10 +2,8 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Windows"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "test" {
@@ -20,7 +18,7 @@ resource "azurerm_linux_web_app" "test" {
   site_config {
     always_on = false
     # application_stack {
-    #   python_version = 3.8
+    #   dotnet_version = "7.0"
     # }
   }
 }
